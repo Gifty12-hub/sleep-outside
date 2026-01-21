8313a4ba0060508820ac2c48ddf2dec3a9279c9b
sirdave01 <d08178084956@gmail.com>
2026-01-21 13:10:09 -0800

    Add customizable alerts from JSON with colors, close button, and auto-dismiss

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import Alert from "./alerts.mjs";
 
 //creating an instance of ProductData class
 const dataSource = new ProductData("tents");
@@ -10,3 +11,8 @@ const productList = new ProductList("tents", dataSource, listElement);
 
 //initializing the product list
 productList.init();
+
+// creating the alert instance
+const alerts = new Alert();
+// Initializing alerts
+alerts.init();
